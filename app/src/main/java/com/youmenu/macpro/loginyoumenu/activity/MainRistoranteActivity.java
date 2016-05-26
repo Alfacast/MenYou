e23068574cbe9e4ad9357014eef1d602d7d81964
Alfacast <progetto.alfacast@gmail.com>
2016-05-26 13:05:43 +0200

    layout menu gestore

--- app/src/main/java/com/youmenu/macpro/loginyoumenu/activity/MainRistoranteActivity.java
+++ app/src/main/java/com/youmenu/macpro/loginyoumenu/activity/MainRistoranteActivity.java
@@ -122,15 +122,11 @@ public class MainRistoranteActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_account) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_menu) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_menu_add) {
 
         } else if (id == R.id.btnLogout) {
                 logoutUser();
