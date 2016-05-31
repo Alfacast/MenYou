7f8402943b0c59f0dcc95005ba05640b72863151
Alfacast <progetto.alfacast@gmail.com>
2016-05-31 09:43:36 +0200

    Progetto rinominato

---
+++ app/src/main/java/com/alfacast/menyou/client/MainClienteActivity.java
@@ -0,0 +1,148 @@
+package com.alfacast.menyou.client;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.alfacast.menyou.login.R;
+import com.alfacast.menyou.login.activity.LoginActivity;
+import com.alfacast.menyou.login.helper.SQLiteHandlerUser;
+import com.alfacast.menyou.login.helper.SessionManager;
+
+import java.util.HashMap;
+
+public class MainClienteActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    private static final String TAG = MainClienteActivity.class.getSimpleName();
+
+    private TextView txtName;
+    private TextView txtEmail;
+
+    private SQLiteHandlerUser db;
+    private SessionManager session;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cliente_main_activity);
+
+
+        txtName = (TextView) findViewById(R.id.name);
+        txtEmail = (TextView) findViewById(R.id.email);
+
+        // SqLite database handler
+        db = new SQLiteHandlerUser(getApplicationContext());
+
+        // session manager
+        session = new SessionManager(getApplicationContext());
+
+        if (!session.isLoggedIn()) {
+            logoutUser();
+        }
+
+
+        // Recuperare dati utente da SQLite
+        HashMap<String, String> user = db.getUserDetails();
+        String name = user.get("name");
+        String email = user.get("email");
+
+
+        Log.d(TAG, "db cliente");
+
+
+        // Displaying the user details on the screen
+        txtName.setText(name);
+        txtEmail.setText(email);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void logoutUser() {
+        session.setLogin(false);
+
+        //dbr.deleteUsers();
+        db.deleteUsers();
+
+        // Launching the login activity
+        Intent intent = new Intent(MainClienteActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_cliente, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_account) {
+            // Handle the camera action
+        } else if (id == R.id.nav_preferiti) {
+
+        } else if (id == R.id.nav_cerca) {
+
+        } else if (id == R.id.btnLogout) {
+
+            logoutUser();
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ app/src/main/java/com/alfacast/menyou/login/helper/SQLiteHandlerRestaurant.java
@@ -0,0 +1,113 @@
+package com.alfacast.menyou.login.helper;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.HashMap;
+
+/**
+ * Created by MacPro on 05/05/16.
+ */
+public class SQLiteHandlerRestaurant extends SQLiteOpenHelper {
+
+    private static final String TAG = SQLiteHandlerRestaurant.class.getSimpleName();
+
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String DATABASE_NAME = "menuYouLoginRistorante";
+
+    private static  final String TABLE_USER = "ristorante";
+
+    private static final String KEY_ID = "id";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_ADDRESS = "indirizzo";
+    private static final String KEY_PARTITAIVA = "partitaIva";
+    private static final String KEY_EMAIL = "email";
+    private static final String KEY_TEL = "telefono";
+    private static final String KEY_UID = "uid";
+    private static final String KEY_CREATED_AT = "created_at";
+
+
+    public SQLiteHandlerRestaurant(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    //creo tabelle
+    @Override
+    public void onCreate(SQLiteDatabase dbr){
+        String CREATE_LOGIN_TABLE = "CREATE TABLE " + TABLE_USER + "("
+                + KEY_ID + "INTEGER PRIMARY KEY," + KEY_NAME + " TEXT,"
+                + KEY_ADDRESS + " TEXT," + KEY_PARTITAIVA + " TEXT UNIQUE,"
+                + KEY_EMAIL + " TEXT UNIQUE," + KEY_TEL + " TEXT UNIQUE,"
+                + KEY_UID + " TEXT," + KEY_CREATED_AT + " TEXT" + ")";
+        dbr.execSQL(CREATE_LOGIN_TABLE);
+
+        Log.d(TAG, "Database restaurant tables created");
+    }
+
+    // aggiornamento database
+    @Override
+    public void onUpgrade(SQLiteDatabase dbr, int oldVersion, int newVersion){
+
+        dbr.execSQL("DROP TABLE IF EXISTS " + TABLE_USER);
+
+        onCreate(dbr);
+    }
+
+    public void addUser(String name, String address, String partitaIva, String email, String tel, String uid, String created_at){
+        SQLiteDatabase dbr = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, name);
+        values.put(KEY_ADDRESS, address);
+        values.put(KEY_PARTITAIVA,partitaIva);
+        values.put(KEY_EMAIL, email);
+        values.put(KEY_TEL, tel);
+        values.put(KEY_UID, uid);
+        values.put(KEY_CREATED_AT, created_at);
+
+        long id = dbr.insert(TABLE_USER, null, values);
+        dbr.close();
+
+        Log.d(TAG, "New user inserted into sqlite: " + id);
+
+    }
+
+
+    public HashMap<String, String> getUserDetails(){
+        HashMap<String, String> user = new HashMap<String, String>();
+        String selectQuery = "SELECT * FROM " + TABLE_USER;
+
+        SQLiteDatabase dbr = this.getReadableDatabase();
+        Cursor cursor = dbr.rawQuery(selectQuery, null);
+
+        cursor.moveToFirst();
+        if(cursor.getCount() >0){
+            user.put("name", cursor.getString(1));
+            user.put("address", cursor.getString(2));
+            user.put("partitaIva", cursor.getString(3));
+            user.put("email", cursor.getString(4));
+            user.put("tel", cursor.getString(5));
+            user.put("uid", cursor.getString(6));
+            user.put("created_at", cursor.getString(7));
+        }
+        cursor.close();
+        dbr.close();
+
+        Log.d(TAG, "Fetching user from Sqlite: " + user.toString());
+        return user;
+    }
+
+    public void deleteUsers(){
+        SQLiteDatabase dbr = this.getWritableDatabase();
+
+        dbr.delete(TABLE_USER, null, null);
+        dbr.close();
+
+        Log.d(TAG, "Deleted all user info from Sqlite");
+    }
+}
