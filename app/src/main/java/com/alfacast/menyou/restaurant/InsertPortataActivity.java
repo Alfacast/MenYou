2968da44d6820c367f59cb63919b10a45f78510c
Alfacast <progetto.alfacast@gmail.com>
2016-06-06 17:44:40 +0200

    inserimento portata completato

--- app/src/main/java/com/alfacast/menyou/restaurant/InsertPortataActivity.java
+++ app/src/main/java/com/alfacast/menyou/restaurant/InsertPortataActivity.java
@@ -1,14 +1,29 @@
 package com.alfacast.menyou.restaurant;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Base64;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.alfacast.menyou.login.R;
@@ -18,10 +33,17 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.facebook.internal.Utility;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,12 +54,16 @@ public class InsertPortataActivity extends AppCompatActivity {
     private static final String TAG = InsertPortataActivity.class.getSimpleName();
     private Button btnInsertPortata;
     private EditText inputNamePortata;
-    private EditText inputCategoriaPortata;
+    private Spinner inputCategoriaPortata;
     private EditText inputDescrizionePortata;
     private EditText inputPrezzoPortata;
     private EditText inputOpzioniPortata;
     private EditText inputDisponibilePortata;
-    private EditText inputFotoPortata;
+    private ImageView viewImage;
+    private Button btnInsertFoto;
+    private Switch switchButton;
+    private String switchOn = "Si";
+    private String switchOff = "No";
     private ProgressDialog pDialog;
     private SQLiteHandlerPortata db;
 
@@ -55,12 +81,33 @@ public class InsertPortataActivity extends AppCompatActivity {
 
         inputNamePortata = (EditText) findViewById(R.id.namePortata);
         btnInsertPortata = (Button) findViewById(R.id.btnInsertPortata);
-        inputCategoriaPortata = (EditText) findViewById(R.id.categoriaPortata);
+        inputCategoriaPortata = (Spinner) findViewById(R.id.categoriaPortata);
         inputDescrizionePortata = (EditText) findViewById(R.id.descrizionePortata);
         inputPrezzoPortata = (EditText) findViewById(R.id.prezzoPortata);
         inputOpzioniPortata = (EditText) findViewById(R.id.opzioniPortata);
         inputDisponibilePortata = (EditText) findViewById(R.id.disponibilePortata);
-        inputFotoPortata = (EditText) findViewById(R.id.fotoPortata);
+        btnInsertFoto=(Button)findViewById(R.id.btnSelectPhoto);
+        viewImage=(ImageView)findViewById(R.id.viewImage);
+        switchButton = (Switch) findViewById(R.id.switchButton);
+
+        switchButton.setChecked(true);
+        switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean bChecked) {
+                if (bChecked) {
+                    inputDisponibilePortata.setText(switchOn);
+                } else {
+                    inputDisponibilePortata.setText(switchOff);
+                }
+            }
+        });
+
+        if (switchButton.isChecked()) {
+            inputDisponibilePortata.setText(switchOn);
+        } else {
+            inputDisponibilePortata.setText(switchOff);
+        }
+
 
         // Progress dialog
         pDialog = new ProgressDialog(this);
@@ -69,16 +116,30 @@ public class InsertPortataActivity extends AppCompatActivity {
         // SQLite database handler
         db = new SQLiteHandlerPortata(getApplicationContext());
 
+        btnInsertFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+
         // Insert Button Click event
         btnInsertPortata.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 String name = inputNamePortata.getText().toString().trim();
-                String categoria = inputCategoriaPortata.getText().toString().trim();
+                String categoria = inputCategoriaPortata.getSelectedItem().toString();
                 String descrizione = inputDescrizionePortata.getText().toString().trim();
                 String prezzo = inputPrezzoPortata.getText().toString().trim();
                 String opzioni = inputOpzioniPortata.getText().toString().trim();
                 String disponibile = inputDisponibilePortata.getText().toString().trim();
-                String foto = inputFotoPortata.getText().toString().trim();
+
+                viewImage.buildDrawingCache();
+                Bitmap bitmap = viewImage.getDrawingCache();
+                ByteArrayOutputStream stream=new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 85, stream);
+                final byte[] image=stream.toByteArray();
+
+                String foto = Base64.encodeToString(image, Base64.NO_WRAP);
 
                 if (!name.isEmpty()) {
                     insertPortata(name, categoria, descrizione, prezzo, opzioni, disponibile, foto);
@@ -90,6 +151,96 @@ public class InsertPortataActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+
+    private void selectImage() {
+
+        final CharSequence[] options = { "Scatta una foto", "Seleziona dalla galleria","Annulla" };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(InsertPortataActivity.this);
+        builder.setTitle("Aggiungi foto");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (options[item].equals("Scatta una foto"))
+                {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    File f = new File(android.os.Environment.getExternalStorageDirectory(), "temp.jpg");
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                    startActivityForResult(intent, 1);
+                }
+                else if (options[item].equals("Seleziona dalla galleria"))
+                {
+                    Intent intent = new   Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intent, 2);
+
+                }
+                else if (options[item].equals("Annulla")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+                File f = new File(Environment.getExternalStorageDirectory().toString());
+                for (File temp : f.listFiles()) {
+                    if (temp.getName().equals("temp.jpg")) {
+                        f = temp;
+                        break;
+                    }
+                }
+                try {
+                    Bitmap bitmap;
+                    BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+
+                    bitmap = BitmapFactory.decodeFile(f.getAbsolutePath(),
+                            bitmapOptions);
+
+                    viewImage.setImageBitmap(bitmap);
+
+                    String path = android.os.Environment
+                            .getExternalStorageDirectory()
+                            + File.separator
+                            + "Phoenix" + File.separator + "default";
+                    f.delete();
+                    OutputStream outFile = null;
+                    File file = new File(path, String.valueOf(System.currentTimeMillis()) + ".jpg");
+                    try {
+                        outFile = new FileOutputStream(file);
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 85, outFile);
+                        outFile.flush();
+                        outFile.close();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else if (requestCode == 2) {
+
+                Uri selectedImage = data.getData();
+                String[] filePath = { MediaStore.Images.Media.DATA };
+                Cursor c = getContentResolver().query(selectedImage,filePath, null, null, null);
+                c.moveToFirst();
+                int columnIndex = c.getColumnIndex(filePath[0]);
+                String picturePath = c.getString(columnIndex);
+                c.close();
+                Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+                Log.w("path of image from gallery......******************.........", picturePath+"");
+                viewImage.setImageBitmap(thumbnail);
+            }
+        }
     }
 
     /**
